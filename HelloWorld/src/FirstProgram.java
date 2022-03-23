6e70153bda1eb17d9ee3ea41ccefe0c01d9caa02
avina <avina@DESKTOP-QHOJUAM>
2022-03-23 17:56:05 +0530

    Added Hello BVC

--- HelloWorld/src/FirstProgram.java
+++ HelloWorld/src/FirstProgram.java
@@ -3,6 +3,8 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello WOrld");
+		System.out.println("Hello SDMIT");
+		System.out.println("Hello BVC");
 
 	}
 
